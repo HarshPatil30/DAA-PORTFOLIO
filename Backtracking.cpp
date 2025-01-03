ad2c73fbce8da0c6ec262bcec65bd329358d7d4d
Harsh Patil <01fe23bcs013@kletech.ac.in>
2025-01-04 01:17:47 +0530

    Update Backtracking.cpp

--- Backtracking.cpp
+++ Backtracking.cpp
@@ -1,50 +1,35 @@
 #include <iostream>
-#include <climits>
-
+#include <vector>
 using namespace std;
 
 #define MAX_INTERSECTIONS 5
 
-bool isValid(int signalTimings[MAX_INTERSECTIONS], int intersection, int currentTiming, int totalTime) {
-    int totalWaitTime = 0;
-    for (int i = 0; i < intersection; i++) {
-        totalWaitTime += signalTimings[i];
-    }
-
-    if (totalWaitTime + currentTiming > totalTime) {
-        return false;
-    }
-    return true;
-}
-
-bool optimizeSignalTimingsHelper(int signalTimings[MAX_INTERSECTIONS], int intersection, int totalTime) {
-    if (intersection == MAX_INTERSECTIONS) {
-        return true;
-    }
-
-    for (int currentTiming = 1; currentTiming <= totalTime; currentTiming++) {
-        if (isValid(signalTimings, intersection, currentTiming, totalTime)) {
-            signalTimings[intersection] = currentTiming;
-            if (optimizeSignalTimingsHelper(signalTimings, intersection + 1, totalTime)) {
-                return true;
-            }
-            signalTimings[intersection] = 0;  // Backtrack
-        }
-    }
-
-    return false;
-}
-
-void optimizeSignalTimings(int totalTime) {
+void optimizeSignalTimings(int trafficDensity[MAX_INTERSECTIONS], int totalTime) {
+    int totalDensity = 0;
     int signalTimings[MAX_INTERSECTIONS] = {0};
 
-    if (optimizeSignalTimingsHelper(signalTimings, 0, totalTime)) {
-        cout << "Optimized signal timings for each intersection:" << endl;
-        for (int i = 0; i < MAX_INTERSECTIONS; i++) {
-            cout << "Intersection " << i + 1 << ": " << signalTimings[i] << " seconds" << endl;
-        }
-    } else {
-        cout << "No valid signal timings found." << endl;
+   
+    for (int i = 0; i < MAX_INTERSECTIONS; i++) {
+        totalDensity += trafficDensity[i];
+    }
+
+    
+    int remainingTime = totalTime;
+    for (int i = 0; i < MAX_INTERSECTIONS; i++) {
+        signalTimings[i] = (trafficDensity[i] * totalTime) / totalDensity;
+        remainingTime -= signalTimings[i];
+    }
+
+    
+    for (int i = 0; remainingTime > 0 && i < MAX_INTERSECTIONS; i++) {
+        signalTimings[i]++;
+        remainingTime--;
+    }
+
+    
+    cout << "Optimized signal timings for each intersection:" << endl;
+    for (int i = 0; i < MAX_INTERSECTIONS; i++) {
+        cout << "Intersection " << i + 1 << ": " << signalTimings[i] << " seconds" << endl;
     }
 }
 
@@ -62,7 +47,7 @@ int main() {
     }
 
     cout << "\nOptimizing signal timings for the intersections..." << endl;
-    optimizeSignalTimings(totalTime);
+    optimizeSignalTimings(trafficDensity, totalTime);
 
     return 0;
 }
